fa7ba05b4add635874c6998b34f2d6e0ad3e8c02
A Vidmar <a.v.vidmar@gmail.com>
2020-01-23 11:42:18 +0100

    assets added

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log("hello world!");
+
+var h = window.innerHeight;
+var w = window.innerWidth;
+
+var site = document.getElementById('site');
+
+var site_h = site.offsetHeight;
+var site_w = site.offsetWidth;
+
+// console.log('screen height = ' + h, 'screen width = ' + w);
+// console.log('site height = ' + site_h, 'site width = ' + site_w);
