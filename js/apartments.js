d6e1c4d373e01474c0057a7d33581bcf4ad82b07
A Vidmar <a.v.vidmar@gmail.com>
2020-02-06 18:32:21 +0100

    basic functionality commit 3

--- js/apartments.js
+++ js/apartments.js
@@ -10,21 +10,27 @@ console.log("hello apartments!");
 var flatOne = document.getElementsByClassName('background-flat-one');
 console.log(flatOne);
 
+var textBox1 = document.querySelector('.text-box--1');
+
 var flatTwo = document.getElementsByClassName('background-flat-two');
 console.log(flatTwo);
+
+var textBox2 = document.querySelector('.text-box--2');
 
 //onMouseEnter
 
 function onMouseEnter(flat) {
     if (flat === 1) {
         for (let i = 0; i < flatOne.length; i++) {
-            flatOne[i].classList.add('visible');
+            flatOne[i].classList.add("visible");
         }
+        textBox1.classList.add("select");
     }
     if (flat === 2) {
         for (let i = 0; i < flatTwo.length; i++) {
-            flatTwo[i].classList.add('visible');
+            flatTwo[i].classList.add("visible");
         }
+        textBox2.classList.add("select");
     }
 }
 
@@ -33,12 +39,14 @@ function onMouseEnter(flat) {
 function onMouseLeave(flat) {
     if (flat === 1) {
         for (let i = 0; i < flatOne.length; i++) {
-            flatOne[i].classList.remove('visible');
+            flatOne[i].classList.remove("visible");
         }
+        textBox1.classList.remove("select");
     }
     if (flat === 2) {
         for (let i = 0; i < flatTwo.length; i++) {
-            flatTwo[i].classList.remove('visible');
+            flatTwo[i].classList.remove("visible");
         }
+        textBox2.classList.remove("select");
     }
 }
